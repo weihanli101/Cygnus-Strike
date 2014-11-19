f0277e823f9c8b78f2deb9a8443994fec4e8569c
Weihan Li <weihanli101@gmail.com>
2014-11-19 11:26:59 -0500

    Awakening 1.3.3 Fixed with hit boxes

--- core/src/com/mygdx/game/HitTest.java
+++ core/src/com/mygdx/game/HitTest.java
@@ -9,9 +9,13 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
 
 
 public class HitTest implements ApplicationListener {
@@ -26,6 +30,9 @@ public class HitTest implements ApplicationListener {
 
     OrthographicCamera camera;
     ShapeRenderer shapeRenderer;
+    Array<Rectangle> ArAliens;
+
+    long lLastSpawnTime;
 
     @Override
     public void create() {
@@ -33,6 +40,7 @@ public class HitTest implements ApplicationListener {
         camera.setToOrtho(true,1200,800);
         batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
+
 
         tAlien = new Texture("Alien.png");
         spAlien = new Sprite(tAlien);
@@ -45,50 +53,62 @@ public class HitTest implements ApplicationListener {
         rectShip.width = 110;
         rectShip.height = 64;
 
+        ArAliens = new Array<Rectangle>();
+        spawnAlien();
+    }
+    public void spawnAlien(){
         rectAlien = new Rectangle();
         rectAlien.x = -200;
-        rectAlien.y = 800/2;
+        rectAlien.y = MathUtils.random(20,780);
         rectAlien.width = 50;
         rectAlien.height = 64;
+        ArAliens.add(rectAlien);
+        lLastSpawnTime = TimeUtils.millis();
     }
-
-
 
     @Override
     public void render() {
         camera.update();
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         batch.setProjectionMatrix(camera.combined);
+
         batch.begin();
-        batch.draw(spSpaceShip,rectShip.x-35,rectShip.y-25);
-        batch.draw(spAlien,rectAlien.x-47,rectAlien.y-68);
+        batch.draw(spSpaceShip, rectShip.x - 35, rectShip.y - 25);
+        for (Rectangle rectAlien : ArAliens) {
+            batch.draw(spAlien, rectAlien.x-47, rectAlien.y-68);
+        }
         batch.end();
-       // shapeRenderer.setProjectionMatrix(camera.combined);
-       // shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-       // shapeRenderer.setColor(Color.GREEN);
-       // shapeRenderer.rect(rectAlien.x, rectAlien.y, rectAlien.getWidth(), rectAlien.getHeight());
-        //shapeRenderer.rect(rectShip.x, rectShip.y, rectShip.getWidth(), rectShip.getHeight());
-        //shapeRenderer.end();
+
+        shapeRenderer.setProjectionMatrix(camera.combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.GREEN);
+        for(Rectangle rectAlien: ArAliens){
+            shapeRenderer.rect(rectAlien.x, rectAlien.y, rectAlien.getWidth(), rectAlien.getHeight());
+        }
+        shapeRenderer.rect(rectShip.x, rectShip.y, rectShip.getWidth(), rectShip.getHeight());
+        shapeRenderer.end();
 
 
-        if(Gdx.input.isTouched()) {
+        if (Gdx.input.isTouched()) {
             Vector3 touchPos = new Vector3();
             touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             camera.unproject(touchPos);
             rectShip.x = touchPos.x - (64 / 2);
-            rectShip.y = touchPos.y -(64/2);
-        }
-        rectAlien.x += 200*Gdx.graphics.getDeltaTime();
-        if(rectAlien.x>1200){
-            rectAlien.x = -200;
-        }
-        if(rectAlien.overlaps(rectShip)){
-            System.out.println("HIT");
-            rectAlien.x = -200;
+            rectShip.y = touchPos.y - (64 / 2);
         }
 
+        if ((TimeUtils.millis() - lLastSpawnTime) > 2000) {
+            spawnAlien();
+        }
+        Iterator<Rectangle> iterator = ArAliens.iterator();
+        while (iterator.hasNext()) {
+            Rectangle rectAlien = iterator.next();
+            rectAlien.x += 200 * Gdx.graphics.getDeltaTime();
+            if (rectAlien.overlaps(rectShip)) {
+                iterator.remove();
+            }
+        }
     }
 
     @Override
